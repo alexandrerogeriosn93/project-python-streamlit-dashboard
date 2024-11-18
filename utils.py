1f7824325144a9a67478cfd2ce1e053a560c0daa
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-18 17:41:17 -0300

    feat: create table revenue by state

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from dataset import data_frame
+
+
 def format_number(value, prefix=""):
     for unit in ["", "mil"]:
         if value < 1000:
@@ -6,3 +9,13 @@ def format_number(value, prefix=""):
         value /= 1000
 
     return f"{prefix} {value:.2f} milhões"
+
+
+data_frame_revenue_by_state = data_frame.groupby("Local da compra")[["Preço"]].sum()
+data_frame_revenue_by_state = (
+    data_frame.drop_duplicates(subset="Local da compra")[
+        ["Local da compra", "lat", "lon"]
+    ]
+    .merge(data_frame_revenue_by_state, left_on="Local da compra", right_index=True)
+    .sort_values("Preço", ascending=False)
+)
