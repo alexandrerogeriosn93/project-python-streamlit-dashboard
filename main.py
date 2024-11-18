4b8fdd39b588cab84a7b7fbe3233a13d6082a012
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-18 14:17:29 -0300

    feat: import dataframe to dashboard

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import streamlit as st
+import plotly.express as px
+from dataset import data_frame
+
+st.set_page_config(layout="wide")
+st.title("Dashboard de Vendas :shopping_trolley:")
+
+first_tab, second_tab, third_tab = st.tabs(["Dataset", "Receita", "Vendedores"])
+
+with first_tab:
+    st.dataframe(data_frame)
