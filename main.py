b5931d82e53c9d399de10c00498eae6a3343afd1
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-18 17:00:01 -0300

    feat: create function to format values

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from dataset import data_frame
+from utils import format_number
 
 st.set_page_config(layout="wide")
 st.title("Dashboard de Vendas :shopping_trolley:")
@@ -15,7 +16,7 @@ with second_tab:
     first_column, second_column = st.columns(2)
 
     with first_column:
-        st.metric("Receita total", data_frame["Preço"].sum())
+        st.metric("Receita total", format_number(data_frame["Preço"].sum(), "R$"))
 
     with second_column:
-        st.metric("Quantidade de vendas", data_frame.shape[0])
+        st.metric("Quantidade de vendas", format_number(data_frame.shape[0]))
