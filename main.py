f4da395f3028fc595f50eaf54a59de38e1365c0c
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-18 16:30:01 -0300

    feat: create metrics

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ first_tab, second_tab, third_tab = st.tabs(["Dataset", "Receita", "Vendedores"])
 
 with first_tab:
     st.dataframe(data_frame)
+
+
+with second_tab:
+    first_column, second_column = st.columns(2)
+
+    with first_column:
+        st.metric("Receita total", data_frame["Preço"].sum())
+
+    with second_column:
+        st.metric("Quantidade de vendas", data_frame.shape[0])
