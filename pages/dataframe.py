d040d7ab837d356c2cea5d4dd6db89ddfd64ad3f
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-19 13:11:42 -0300

    feat: create filters to select data by column, category, date and price

--- pages/dataframe.py
+++ pages/dataframe.py
@@ -2,4 +2,37 @@ import streamlit as st
 from dataset import data_frame
 
 st.title("Dataset de Vendas")
-st.dataframe(data_frame)
+
+with st.expander("Colunas"):
+    columns = st.multiselect(
+        "Selecione as colunas", list(data_frame.columns), list(data_frame.columns)
+    )
+
+st.sidebar.title("Filtros")
+
+with st.sidebar.expander("Categoria do Produto"):
+    categories = st.multiselect(
+        "Selecione as categorias",
+        data_frame["Categoria do Produto"].unique(),
+        data_frame["Categoria do Produto"].unique(),
+    )
+
+with st.sidebar.expander("Preço do Produto"):
+    price = st.slider("Selecione o Preço", 0, 5000, (0, 5000))
+
+with st.sidebar.expander("Data da Compra"):
+    date_sale = st.date_input(
+        "Selecione a data",
+        (data_frame["Data da Compra"].min(), data_frame["Data da Compra"].max()),
+    )
+
+query = """
+    `Categoria do Produto` in @categories and \
+    @price[0] <= `Preço` <= @price[1] and \
+    @date_sale[0] <= `Data da Compra` <= @date_sale[1]
+"""
+
+filter_data = data_frame.query(query)
+filter_data = filter_data[columns]
+
+st.dataframe(filter_data)
